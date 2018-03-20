cb02bd8b1558742d3ea93247f617d041253c69ad
Michael Bortlik <Michael.Bortlik@gmail.com>
2018-03-20 22:21:41 +0100

    complete one error left

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-cd ~/eco_server && mono EcoServer.exe -nogui
+ln -sf ${ECO_STORAGE_FOLDER} ${ECO_SERVER_FILES}/Storage
+ln -sf ${ECO_CONFIG_FOLDER} ${ECO_SERVER_FILES}/Config
+
+cd ${ECO_SERVER_FILES} 
+mono EcoServer.exe -nogui
